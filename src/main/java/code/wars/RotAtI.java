d83eaaa0201ce62dbc71bb74ae289cb384d4178f
Billy Hardy <hardywb@leidos.com>
2017-02-08 17:27:21 -0500

    tests pass

--- src/main/java/code/wars/RotAtI.java
+++ src/main/java/code/wars/RotAtI.java
@@ -3,7 +3,30 @@ package code.wars;
 public class RotAtI {
 
     public int exec(int num) {
-        return num;
+        int max = num;
+        String s = num + "";
+        for(int i = 0; i < s.length()-1; i++) {
+            int next = Integer.parseInt(rotAtI(s, i));
+            if(next > max) {
+                max = next;
+            }
+            s = next + "";
+        }
+        return max;
+    }
+
+    public String rotAtI(String num, int i) {
+        String s = num + "";
+        String temp = s.substring(0, i) + s.substring(i+1) + s.substring(i, i+1);
+        return temp;
+    }
+
+    public int pow(int base, int exp) {
+        int power = 1;
+        for(int i = 0; i < exp; i++) {
+            power *= base;
+        }
+        return power;
     }
 
 }
