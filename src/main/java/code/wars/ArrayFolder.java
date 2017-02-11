3c6b1f4bb6a852d973416fe64aac8ffd61a97731
Billy Hardy <hardywb@leidos.com>
2017-02-11 15:16:14 -0500

    fix array folder tests

---
+++ src/main/java/code/wars/ArrayFolder.java
@@ -0,0 +1,15 @@
+package code.wars;
+
+public class ArrayFolder {
+    
+    public int[] fold(int[] arr) {
+        int[] ret = new int[(arr.length+1)/2];
+        for(int i=0; i<arr.length/2; i++) {
+            ret[i] = arr[i] + arr[arr.length-1-i];
+        }
+        if(arr.length%2 == 1) {
+            ret[ret.length] = arr[ret.length];
+        }
+        return ret;
+    }
+}
