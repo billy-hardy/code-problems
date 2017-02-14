5ceb98d76352851fc291aa7bae8488616fd83168
Billy Hardy <hardywb@leidos.com>
2017-02-14 12:11:02 -0500

    add test for multiply array indices except current index

---
+++ src/main/java/code/wars/MultiplyArrayExceptI.java
@@ -0,0 +1,7 @@
+package code.wars;
+
+public class MultiplyArrayExceptI {
+    public int[] exec(int[] arr) {
+        return arr;
+    }
+}
---
+++ src/test/java/code/wars/MultiplyArrayExceptITest.java
@@ -0,0 +1,45 @@
+package code.wars;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import code.wars.MultiplyArrayExceptITest;
+
+@RunWith(Parameterized.class)
+public class MultiplyArrayExceptITest {
+
+    public MultiplyArrayExceptI multiplier;
+    public int[] input;
+    public int[] expected;
+
+    public MultiplyArrayExceptITest(int[] input, int[] expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+
+    @Parameters
+    public static Collection<int[][]> data() {
+        return Arrays.asList(new int[][][] {
+            {{1,2,3,4}, {24, 12, 8, 6}}
+        });
+    }
+
+    @Before
+    public void setup() {
+        multiplier = new MultiplyArrayExceptI();
+    }
+
+    @Test
+    public void testMultiplyArrayExceptI() {
+        int[] actual = multiplier.exec(input);
+        assertArrayEquals(expected, actual);
+    }
+
+}
