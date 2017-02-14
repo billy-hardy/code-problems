026c2ce5096db7668b26173066414c3eb664ba16
Billy Hardy <hardywb@leidos.com>
2017-02-14 13:13:34 -0500

    tests pass and solution is in linear time without division

--- src/main/java/code/wars/MultiplyArrayExceptI.java
+++ src/main/java/code/wars/MultiplyArrayExceptI.java
@@ -2,6 +2,24 @@ package code.wars;
 
 public class MultiplyArrayExceptI {
     public int[] exec(int[] arr) {
-        return arr;
+        int[] front = new int[arr.length];
+        int[] back = new int[arr.length];
+        for(int i=0; i<arr.length; i++) {
+            front[i] = 1;
+            back[i] = 1;
+        }
+        front[0] = arr[0];
+        back[0] = arr[arr.length-1];
+        for(int i=1; i<arr.length; i++) {
+            front[i] *= front[i-1] * arr[i];
+            back[i] *= back[i-1] * arr[arr.length-1-i];
+        }
+        int[] ret = new int[arr.length];
+        for(int i=0; i<ret.length-1; i++) {
+            ret[i+1] = front[i] * back[ret.length-1-i];
+        }
+        ret[0] = back[ret.length-1-1];
+        ret[ret.length-1] = front[ret.length-1-1];
+        return ret;
     }
 }
