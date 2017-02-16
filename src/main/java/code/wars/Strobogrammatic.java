1b5b8fb8e9e38cadedf7f30d90fc0a9d11641411
Billy Hardy <hardywb@leidos.com>
2017-02-16 14:28:23 -0500

    rename map to mirror

--- src/main/java/code/wars/Strobogrammatic.java
+++ src/main/java/code/wars/Strobogrammatic.java
@@ -7,15 +7,15 @@ import java.util.Arrays;
 
 public class Strobogrammatic {
 
-    private Map<String, String> validTransforms;
+    private Map<String, String> mirror;
 
     public Strobogrammatic() {
-        this.validTransforms = new HashMap<String, String>();
-        this.validTransforms.put("0", "0");
-        this.validTransforms.put("1", "1");
-        this.validTransforms.put("6", "9");
-        this.validTransforms.put("8", "8");
-        this.validTransforms.put("9", "6");
+        this.mirror = new HashMap<String, String>();
+        this.mirror.put("0", "0");
+        this.mirror.put("1", "1");
+        this.mirror.put("6", "9");
+        this.mirror.put("8", "8");
+        this.mirror.put("9", "6");
     }
 
     public boolean exec(int num) {
@@ -29,9 +29,9 @@ public class Strobogrammatic {
         if(num.length() == 1) {
             return num.equals("0") || num.equals("1") || num.equals("8");
         }
-        String leftMost = num.substring(0,1);
+        String leftMost = num.substring(0, 1);
         String rightMost = num.substring(num.length()-1);
-        if(this.validTransforms.get(leftMost).equals(rightMost)) {
+        if(this.mirror.get(leftMost).equals(rightMost)) {
             return this.exec(num.substring(1, num.length()-1));
         }
         return false;
