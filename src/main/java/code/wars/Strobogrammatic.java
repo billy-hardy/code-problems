7ce3712fbcd220eaff7b61f22c216d548b32465f
Billy Hardy <hardywb@leidos.com>
2017-02-16 14:25:16 -0500

    string solution works

--- src/main/java/code/wars/Strobogrammatic.java
+++ src/main/java/code/wars/Strobogrammatic.java
@@ -7,15 +7,15 @@ import java.util.Arrays;
 
 public class Strobogrammatic {
 
-    private Map<String, List<String>> validTransforms;
+    private Map<String, String> validTransforms;
 
     public Strobogrammatic() {
-        this.validTransforms = new HashMap<String, List<String>>();
-        this.validTransforms.put("0", Arrays.asList("0"));
-        this.validTransforms.put("1", Arrays.asList("1"));
-        this.validTransforms.put("6", Arrays.asList("9"));
-        this.validTransforms.put("8", Arrays.asList("8"));
-        this.validTransforms.put("9", Arrays.asList("6"));
+        this.validTransforms = new HashMap<String, String>();
+        this.validTransforms.put("0", "0");
+        this.validTransforms.put("1", "1");
+        this.validTransforms.put("6", "9");
+        this.validTransforms.put("8", "8");
+        this.validTransforms.put("9", "6");
     }
 
     public boolean exec(int num) {
@@ -27,12 +27,12 @@ public class Strobogrammatic {
             return true;
         }
         if(num.length() == 1) {
-            return this.validTransforms.containsKey(num);
+            return num.equals("0") || num.equals("1") || num.equals("8");
         }
         String leftMost = num.substring(0,1);
         String rightMost = num.substring(num.length()-1);
-        if(this.validTransforms.get(leftMost).contains(rightMost)) {
-            return this.exec(num.substring(1,num.length()));
+        if(this.validTransforms.get(leftMost).equals(rightMost)) {
+            return this.exec(num.substring(1, num.length()-1));
         }
         return false;
     }
