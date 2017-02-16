c80674faf4228e5ba15eb553075480f446e6b18d
Billy Hardy <hardywb@leidos.com>
2017-02-16 13:56:26 -0500

    fix tests for multiply array and cleaner solution

--- src/main/java/code/wars/MultiplyArrayExceptI.java
+++ src/main/java/code/wars/MultiplyArrayExceptI.java
@@ -4,22 +4,16 @@ public class MultiplyArrayExceptI {
     public int[] exec(int[] arr) {
         int[] front = new int[arr.length];
         int[] back = new int[arr.length];
-        for(int i=0; i<arr.length; i++) {
-            front[i] = 1;
-            back[i] = 1;
-        }
-        front[0] = arr[0];
-        back[0] = arr[arr.length-1];
+        front[0] = 1;
+        back[arr.length-1] = 1;
         for(int i=1; i<arr.length; i++) {
-            front[i] *= front[i-1] * arr[i];
-            back[i] *= back[i-1] * arr[arr.length-1-i];
+            front[i] = front[i-1] * arr[i-1];
+            back[arr.length-i-1] = back[arr.length-i] * arr[arr.length-i];
         }
         int[] ret = new int[arr.length];
-        for(int i=0; i<ret.length-1; i++) {
-            ret[i+1] = front[i] * back[ret.length-1-i];
+        for(int i=0; i<ret.length; i++) {
+            ret[i] = front[i] * back[i];
         }
-        ret[0] = back[ret.length-1-1];
-        ret[ret.length-1] = front[ret.length-1-1];
         return ret;
     }
 }
