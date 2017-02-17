ba10d7165e11233f7a65d34eb6a3e3c606f8dc96
Billy Hardy <hardywb@leidos.com>
2017-02-17 18:44:37 -0500

    fix log and pow problems

--- src/main/java/code/wars/LeftMostBinaryTree.java
+++ src/main/java/code/wars/LeftMostBinaryTree.java
@@ -5,25 +5,25 @@ import code.wars.structures.BinaryTree;
 public class LeftMostBinaryTree {
 
     public int log(int num) {
-        return (int) (Math.log(num)/Math.log(2));
+        int log = (int) (Math.log(num)/Math.log(2));
+        if((int) Math.pow(2, log) == num) {
+            return log+1;
+        }
+        return log;
     }
-    
+
     public int pow(int base, int exp) {
         if(exp <= 0) {
             return 1;
-        }
-        if(exp == base) {
-            return base;
         }
         return base*pow(base, exp-1);
     }
 
     public Integer find(BinaryTree tree) {
-        int ret = 1;
-        int lastLevelNum = this.log(tree.capacity())-1;
+        int lastLevelNum = this.log(tree.capacity())-2;
         int rowStart = this.pow(2, lastLevelNum-1)-1;
-        int rowEnd = this.pow(2, lastLevelNum)-1;
-        for (int i=rowStart; i<=rowEnd; i++) {
+        int rowEnd = this.pow(2, lastLevelNum);
+        for (int i=rowStart; i<rowEnd; i++) {
             Integer curr = tree.getLeft(i);
             if(curr != null) {
                 return curr;
@@ -33,6 +33,6 @@ public class LeftMostBinaryTree {
                 return curr;
             }
         }
-        return ret;
+        return null;
     }
 }
